5f8349e62a940fc3f7001614c7211751939ccab1
Mateus Elias Vieira <mateus.elias.vieira@hotmail.com>
2025-08-29 18:50:52 -0300

    first commit

---
+++ src/main/java/com/delivery/delivery_ms_bff/DeliveryMsBffApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.delivery_ms_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMsBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryMsBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivery/delivery_ms_bff/DeliveryMsBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.delivery_ms_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMsBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
